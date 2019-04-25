d211cdfc226cf834aa55d2b211656e329518648c
Sergey <sergey100717@gmail.com>
2019-04-25 23:55:47 +0300

    1
    Добавлены базовые классы

---
+++ Model/DiscountRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDiscount.Model
+{
+    class DiscountRule
+    {
+        private Product[] items;
+        private int discountPersent;
+        //TODO: возможно не понадобится
+        private int priority;
+
+        public DiscountRule(int discountPersent, Product[] items)
+        {
+            this.discountPersent = discountPersent;
+            this.items = items;
+        }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDiscount.Model
+{
+    class Product
+    {
+        private string name;
+        private double cost;
+
+        public Product(string name, double cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
